1a7e3b6997f149ce5d9253a07225beb1ea7a9481
hyuk0309 <bahngeh@naver.com>
2022-03-12 18:46:25 +0900

    학습테스트 실습 요구사항 3 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,16 +1,20 @@
 package study;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
 
+    @DisplayName("split 테스트")
     @Test
     void split() {
         //given
@@ -21,6 +25,7 @@ public class StringTest {
         assertThat(actual).contains("1", "2");
     }
 
+    @DisplayName("split 일어나지 않는 경우 테스트")
     @Test
     void noSplit() {
         //given
@@ -31,6 +36,7 @@ public class StringTest {
         assertThat(actual).containsExactly("1");
     }
 
+    @DisplayName("substring() 테스트")
     @Test
     void substring() {
         //given
@@ -39,6 +45,32 @@ public class StringTest {
 
         //then
         assertThat(actual).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt 정상동작 테스트")
+    @RepeatedTest(value = 3, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    void charAt() {
+        //given
+        int randomIdx = (int)(Math.random() * 10) % 3;
+        char[] charArr = "abc".toCharArray();
+
+        //when
+        char actual = "abc".charAt(randomIdx);
+
+        //then
+        assertThat(actual).isEqualTo(charArr[randomIdx]);
+    }
+
+    @DisplayName("charAt outOfBound test")
+    @Test
+    void chatAtOutOfBound() {
+        //given
+        int index = "abc".length();
+
+        //when
+        //then
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> "abc".charAt(index));
     }
 
 }
