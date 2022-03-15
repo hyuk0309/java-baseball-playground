aefb1fa2d2cb66d72145ff9cd9eab45db4231581
hyuk0309 <bahngeh@naver.com>
2022-03-15 14:21:51 +0900

    feat : 게임결과 출력 기능 구현

--- src/main/java/numberbaseballgame/View.java
+++ src/main/java/numberbaseballgame/View.java
@@ -12,4 +12,23 @@ public class View {
         return input;
     }
 
+    public void output(GameResult gameResult) {
+        if(gameResult.getStrike() == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return;
+        }
+        if(gameResult.getBall() == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        String msg = "";
+        if(gameResult.getBall() > 0) {
+            msg += gameResult.getBall() + "볼 ";
+        }
+        if(gameResult.getStrike() > 0) {
+            msg += gameResult.getStrike() + "스트라이크";
+        }
+        System.out.println(msg);
+    }
 }
