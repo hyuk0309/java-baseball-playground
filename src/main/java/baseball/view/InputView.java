cf6d8f97b572493629ec883b6da6b1eb42a4518f
hyuk0309 <bahngeh@naver.com>
2022-03-17 14:24:36 +0900

    feat : 사용자 난수 입력받기 기능 개발

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,40 @@
+package baseball.view;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    public List<Integer> inputNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        String numbersString = scanner.nextLine();
+
+        validateRegex(numbersString);
+        validateDuplicate(numbersString);
+
+        return translateInput(numbersString);
+    }
+
+    private List<Integer> translateInput(String numbersString) {
+        return numbersString.chars()
+            .mapToObj(c -> Character.getNumericValue(c))
+            .collect(Collectors.toList());
+    }
+
+    private void validateRegex(String numbersString) {
+        if (!numbersString.matches("[1-9]{3}")) {
+            throw new IllegalStateException("1-9사이의 숫자 3개를 입력해주세요.");
+        }
+    }
+
+    private void validateDuplicate(String numbersString) {
+        int numberCount = numbersString.chars()
+            .boxed()
+            .collect(Collectors.toSet())
+            .size();
+        if(numberCount != 3) {
+            throw new IllegalStateException("숫자에 중복이 없게 입력해주세요.");
+        }
+    }
+}
